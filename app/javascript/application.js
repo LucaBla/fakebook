bb4bfd5fe33636903e48bd932032b2f19d82b3a1
Luca <lucablazevic@yahoo.de>
2022-07-14 23:56:39 +0200

    add fade-in and -out animation for comments

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,22 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener('turbo:before-stream-render', function(event) {
+  if(document.getElementById(event.target.target).classList.contains('comment-wrapper')){
+    event.preventDefault()
+
+    let oldElement = document.getElementById(event.target.target)
+  
+    if(event.target.action === 'remove'){
+      oldElement.classList.add('comment-out')
+    }
+  
+    oldElement.addEventListener('animationend', function() {
+      oldElement.replaceWith('')
+    })
+  }
+  else if(event.target.firstElementChild instanceof HTMLTemplateElement){
+    event.target.templateElement.content.firstElementChild.classList.add('comment-in')
+  }
+})
