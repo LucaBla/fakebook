2eb06df7df89ea76b6b9395af288faa3bb4e2359
Luca <lucablazevic@yahoo.de>
2022-07-14 06:03:50 +0200

    add textoverflow ellipsis to usernames

--- app/javascript/controllers/request_controller.js
+++ app/javascript/controllers/request_controller.js
@@ -1,13 +1,15 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ 'pop' ]
+  static targets = [ 'pop', 'buttons' ]
 
   decline() {
     this.popTarget.classList.add('declined')
+    this.buttonsTarget.classList.add('hidden')
   }
 
   accept(){
     this.popTarget.classList.add('accepted')
+    this.buttonsTarget.classList.add('hidden')
   }
 }
