57d67f67a2dc5b0e21ffc90d625a6cee96361df2
Luca <lucablazevic@yahoo.de>
2022-07-26 04:33:11 +0200

    add new post modal

--- app/javascript/controllers/application_controller.js
+++ app/javascript/controllers/application_controller.js
@@ -1,9 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ 'dropDown' ]
+  static targets = [ 'dropDown', 'modal' ]
 
   show() {
     this.dropDownTarget.classList.toggle('hidden')
+  }
+
+  showModal(){
+    this.modalTarget.classList.toggle('hidden')
   }
 }
